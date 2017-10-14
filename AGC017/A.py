1f48c781742254a5a6fc5ffccfbf8b742fbd8e5d
tonchan1216 <cleartone1216@gmail.com>
2017-10-14 19:40:21 +0900

    AGC017 A 三項

--- AGC017/A.py
+++ AGC017/A.py
@@ -2,9 +2,9 @@ input1 = list(map(int,input("").split(" ")))
 A = list(map(int,input("").split(" ")))
 N = input1[0]
 P = input1[1]
-
 even = []
 odd = []
+
 for a in A:
 	if a % 2 == 0:
 		even.append(a)
@@ -12,10 +12,7 @@ for a in A:
 		odd.append(a)
 
 if len(odd) == 0:
-	if P == 0:
-		result = 2**N
-	else:
-		result = 0
+	result = 2**N if (P==0) else 0
 else:
 	result = 2**len(even) * 2**(len(odd)-1)
 
