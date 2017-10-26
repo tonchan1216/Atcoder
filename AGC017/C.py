11182105733e3b811619745d488cf8caf4add244
tonchan1216 <cleartone1216@gmail.com>
2017-10-26 20:34:47 +0900

    AGC017

--- AGC017/C.py
+++ AGC017/C.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 input1 = list(map(int,input("").split(" ")))
 A = list(map(int,input("").split(" ")))
 N = input1[0]
@@ -7,18 +9,29 @@ XY = [None] * M
 for i in range(M):
  	XY[i] = list(map(int,temp[i].split(" ")))
 
-for j in range(M):
-	A[XY[j][0]-1] = XY[j][1]
-	sorted(A, reverse=True)
-	magic = [None] * N
-	for ball in A:
-		index = 1
-		while (ball-index) >= 0:
-			if magic[ball-index] == None:
-				magic[ball-index] = ball
-				break
-			elif magic[ball-index] == ball:
-				index += 1
-			else:
-				break
-	print(magic.count(None))
+A_uniq = list(set(A))
+magic = {}
+# O(N)
+for ball in A_uniq:
+	ball_num = A.count(ball)
+	magic[ball] = list(range((ball-ball_num+1), ball+1))
+
+print(magic)
+
+# O(M)
+for m in range(M):
+	i = A[XY[m][0]-1] #before
+	j = XY[m][1] #after
+	if len(magic[i]) > 1:
+		magic[i].pop(0)
+	else:
+		del magic[i]
+	if j in magic :
+		magic[j].insert(0, min(magic[j])-1)
+	else:
+		magic[j] = [j]
+	print(magic)
+	temp = [flatten for inner in magic.values() for flatten in inner]
+	#temp = map(lambda x: x+temp, magic.values())
+	magic_uniq = list(set(temp))
+	#print(len(temp) - len(magic_uniq))
